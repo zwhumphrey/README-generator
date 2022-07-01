612e9839418bf99a6e85c0bd032a3178b582a103
zwhumphrey <103855578+zwhumphrey@users.noreply.github.com>
2022-07-01 17:50:50 -0500

    inital start up code

---
+++ index.js
@@ -0,0 +1,19 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown.js");
+
+console.log("Answer the following questions and you will be provided with a high quality README file!")
+// An array of questions for user input
+const questions = [
+    
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
