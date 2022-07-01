f56e17f79f04fbffd05a4d0943048dd477f8d231
zwhumphrey <103855578+zwhumphrey@users.noreply.github.com>
2022-07-01 18:10:38 -0500

    this is the start of the function to create the readme

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
-const generateMarkdown = require("./utils/generateMarkdown.js");
+const generateMarkdown = require("./src/generateMarkdown.js");
 
 console.log("Answer the following questions and you will be provided with a high quality README file!")
 // An array of questions for user input
 const questions = [
-    
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?",
+        validate: answer => {
+            if (answer) {
+                return true;
+            } else {
+                console.log("You did not enter a title please do so to continue!");
+                return false;
+            }
+        }
+    },
+
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("You have successfully created a README file!")
+    });
+};
 
-// TODO: Create a function to initialize app
+// A function to initialize app
 function init() {}
 
 // Function call to initialize app
